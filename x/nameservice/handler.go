e81dec0b041c1e1d5cad59e7eb37c6d3ffbf0a31
link.messagebox <link.messagebox@gmail.com>
2022-12-15 09:03:07 -0500

    Add: a test version to implement basic concept functions

---
+++ x/nameservice/handler.go
@@ -0,0 +1,55 @@
+package nameservice
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// NewHandler returns a handler for "nameservice" type message.
+func NewHandler(keeper Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
+		switch msg := msg.(type) {
+		case MsgSetName:
+			return handleMsgSetName(ctx, keeper, msg)
+		case MsgBuyName:
+			return handleMsgBuyName(ctx, keeper, msg)
+		default:
+			errMsg := fmt.Sprintf("Unecognized nameservice Msg type: %v", msg.Type())
+			return sdk.ErrUnknownRequest(errMsg).Result()
+		}
+
+	}
+}
+
+// Handle a message to set name
+func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) sdk.Result {
+	if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) {
+		return sdk.ErrUnauthorized("Incorrect Owner").Result()
+	}
+
+	keeper.SetName(ctx, msg.Name, msg.Value)
+	return sdk.Result{}
+}
+
+// Handle a message to buy name
+func handleMsgBuyName(ctx sdk.Context, keeper Keeper, msg MsgBuyName) sdk.Result {
+	if keeper.GetPrice(ctx, msg.Name).IsAllGT(msg.Bid) {
+		return sdk.ErrInsufficientCoins("Bid not high enough").Result()
+	}
+
+	if keeper.HasOwner(ctx, msg.Name) {
+		_, err := keeper.coinKeeper.SendCoins(ctx, msg.Buyer, keeper.GetOwner(ctx, msg.Name), msg.Bid)
+		if err != nil {
+			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
+		}
+	} else {
+		_, _, err := keeper.coinKeeper.SubtractCoins(ctx, msg.Buyer, msg.Bid)
+		if err != nil {
+			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
+		}
+	}
+	keeper.SetOwner(ctx, msg.Name, msg.Buyer)
+	keeper.SetPrice(ctx, msg.Name, msg.Bid)
+	return sdk.Result{}
+}
